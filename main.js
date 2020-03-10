79b0f42b3a6fc8bec96f85480adcdc73edc6fec3
robertkoch117 <robertkoch117@gmail.com>
2020-03-10 09:32:24 -0300

    final course axios

--- main.js
+++ main.js
@@ -1,35 +1,6 @@
-/*var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://api.github.com/users/diego3g');
-xhr.send(null);
-
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
-}*/
-
-var minhaPromise = function(){
-    return new Promise(function( resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://api.github.com/users/diego3g');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                }else{
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/robertkoch117')
     .then(function(response){
-        console.log(response);
+        console.log(response.data.avatar_url);
     })
     .catch(function(error){
         console.warn(error)
